176fd58043f5ec30fe76433bfa54f9b55d228959
너 깃 이름 < >
2021-09-28 21:20:15 +0900

    과제 1

---
+++ s0928/s0928.js
@@ -0,0 +1,8 @@
+document.getElementById("out").innerHTML = "<pre>// html요소로 출력 작성 \ndocument.getElementById(\"demo\").innerHTML</pre>";
+
+document.write("<pre>// document 출력으로 작성 \ndocument.getElementById(\"demo\").innerHTML</pre>");
+
+window.alert("// window 객체 경고창을 사용하여 출력 \nwindow.alert()");
+
+let logWrite = "// 브라우저 콘솔을 사용하여 출력 =>> console.log()";
+console.log(logWrite);
